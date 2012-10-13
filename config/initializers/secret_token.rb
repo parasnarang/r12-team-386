998eade880cbf19ef6e94d16c683c5fa9e0b6092
Rohit Mishra <rohit@rohitmishra.me>
2012-10-13 14:41:24 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialkart::Application.config.secret_token = '616d405472e87f1069cc8fb3c1fe4dc5c12bbab74498feb10144bb3c575dc3008d16e89efe7dbdb26dc025410503da3dec1ff7cd0792087a5e29a4fabca86f0d'
